6f7edab5a03475f715e854f7a97082da2c3ad380
aashraful349 <aashraful349@gmail.com>
2026-02-19 09:50:04 +0600

    header completed

---
+++ script/index.js
@@ -0,0 +1,11 @@
+const loadLessons=()=>{
+    fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then(res=>res.json())
+    .then(json=>displayLesson(json.data))
+}
+
+const displayLesson=(lessons)=>{
+    console.log(lessons);
+}
+
+loadLessons();
