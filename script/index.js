94fbba3f41b9c4a9fdb5b4eb003f6d89110d32df
aashraful349 <aashraful349@gmail.com>
2026-02-20 08:37:06 +0600

    completed modal

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
+const createElements=(arr)=>{
+    const htmlElements=arr.map((el)=>`<span class="btn">${el}</span>`);
+    return(htmlElements.join(" "));
+}
+
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(res => res.json())
@@ -10,7 +16,6 @@ const removeActive=()=>{
     lessonButtons.forEach(btn=>btn.classList.remove("active"));
 }
 
-
 const loadLevelWord=(id)=>{
     // console.log(id);
     const url =`https://openapi.programming-hero.com/api/level/${id}`;
@@ -22,6 +27,38 @@ const loadLevelWord=(id)=>{
         // console.log(clickBtn);
         clickBtn.classList.add("active");
         displayLevelWord(data.data)})
+}
+
+const loadWordDetail=async(id)=>{
+    const url=`https://openapi.programming-hero.com/api/word/${id}`;
+    // console.log(url);
+    const res=await fetch(url);
+    const details=await res.json();
+    // console.log(details.data);
+    displayWordDetails(details.data);
+};
+
+const displayWordDetails=(word)=>{
+    // console.log(word);
+    const detailsBox=document.getElementById("details-container");
+    detailsBox.innerHTML=`
+    <div class="">
+                    <h2 class="text-2xl font-bold">${word.word}(<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+                </div>
+                <div class="">
+                    <h2 class="font-bold">Meaning</h2>
+                    <p>${word.meaning}</p>
+                </div>
+                <div class="">
+                    <h2 class="text-2xl font-bold">Example</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="">
+                    <h2 class="text-2xl font-bold">Synonyms</h2>
+                    <div class="">${createElements(word.synonyms)}</div>
+                </div>
+    `;
+    document.getElementById("word_modal").showModal();
 }
 
 const displayLevelWord=(words)=>{
@@ -47,7 +84,7 @@ const displayLevelWord=(words)=>{
             <p class="font-semibold">Meaning /Pronounciation</p>
             <div class="text-2xl font-medium font-bangla">"${word.meaning?word.meaning:"Meaning Not Available"} / ${word.pronunciation?word.pronunciation:"Pronunciation Not Available"}"</div>
             <div class="flex justify-between items-center">
-                <button onClick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onClick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
